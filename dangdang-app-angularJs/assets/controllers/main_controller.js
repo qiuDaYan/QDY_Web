1ea65bcd18594ddacb5702696dd9c1581e528dbd
qiuDaYan <tangchenaqy@163.com>
2016-10-19 18:02:12 +0800

    1.0.4.6
    last

--- dangdang-app-angularJs/assets/controllers/main_controller.js
+++ dangdang-app-angularJs/assets/controllers/main_controller.js
@@ -2,6 +2,7 @@ app.controller('mainCtroller', ['$scope', 'commonService', function($scope, comm
     commonService.getData('guji', function(res) {
         console.dir(res);
     });
+    $scope.selectedIndex=0;
     $scope.linkTo =function(a){
       $scope.selectedIndex=a;
     };
