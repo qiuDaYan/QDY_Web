19ae1757a4e6e89132c8ec6bfbc08f0e1b1aa32e
qiuDaYan <tangchenaqy@163.com>
2016-10-20 09:44:30 +0800

    1.0.5.1
    fix BUG

--- dangdang-app-angularJs/assets/controllers/main_controller.js
+++ dangdang-app-angularJs/assets/controllers/main_controller.js
@@ -1,7 +1,8 @@
-app.controller('mainController', ['$scope', 'commonService', function($scope, commonService) {
-    commonService.getData('guji', function(res) {
-        console.dir(res);
-    });
+app.controller('mainController', ['$scope', function($scope) {
+    // commonService.getData('guji', function(res) {
+    //     console.dir(res);
+    // });
+    // 导航显示
     $scope.selectedIndex = 0;
     $scope.linkTo = function(index) {
         $scope.selectedIndex = index;
