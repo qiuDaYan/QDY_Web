f99af861750851a05c2857f030a5ca5ba4c406e2
qiuDaYan <tangchenaqy@163.com>
2016-10-26 09:30:06 +0800

    1.0.5.7
    路径修改

--- dangdang-app-angularJs/assets/api/service.js
+++ dangdang-app-angularJs/assets/api/service.js
@@ -3,7 +3,7 @@ app.factory('commonService', ['$http', function($http) {
     // 根据分类信息取数据
     service.getData = function(type, callBack) {
         $http({
-            url: '/dangdang-app-angularJs/data/' + type + '.json',
+            url: '/QDY_Web/dangdang-app-angularJs/data/' + type + '.json',
             method: 'get'
         }).then(function(res) {
             console.log('数据获取到了');
