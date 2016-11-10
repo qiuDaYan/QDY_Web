e0c3bf855162d886b519a66db7aaaf09c61910c8
qiuDaYan <tangchenaqy@163.com>
2016-11-10 13:41:01 +0800

    换电脑
    上传

--- dangdang-app-angularJs/assets/controllers/book_listCtrl.js
+++
@@ -1,7 +0,0 @@
-app.controller('booklistCtrl', ['$scope', 'commonService','$routeParams', function($scope, commonService,$routeParams) {
-    commonService.getData($routeParams.id ,function(res) {
-      console.log('1');
-      console.log(res.data);
-      $scope.bookData=res.data;
-    });
-}]);
--- dangdang-app-angularJs/assets/controllers/main_controller.js
+++
@@ -1,6 +0,0 @@
-app.controller('mainController', ['$scope', function($scope) {
-    $scope.selectedIndex = 0;
-    $scope.linkTo = function(index) {
-        $scope.selectedIndex = index;
-    };
-}]);
